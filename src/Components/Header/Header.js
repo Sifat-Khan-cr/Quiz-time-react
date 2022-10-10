38dcb67c33aa66afdbdcc05e654a5cf5e3a63e7c
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-10-10 23:45:36 +0600

    header data loded

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
             <div className='grid grid-cols-1 gap-4 justify-center md:grid-cols-4 lg:px-20 py-5 mx-auto'>
                 {
                     topics.map(topic => <QuizTopic key={topic.id} topic={topic}></QuizTopic>)
-                    // topics.map(topic => console.log(topic))
+                    topics.map(topic => console.log(topic))
                 }
 
             </div>
