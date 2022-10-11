6ecb464919f49ba7a33ace82e1dc17ddf2018813
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-10-11 22:03:42 +0600

    quiz picture added

--- src/Components/MainQuiz/MainQuiz.js
+++ src/Components/MainQuiz/MainQuiz.js
@@ -8,9 +8,11 @@ const MainQuiz = () => {
 
     return (
         <div>
-            <h1 className='text-6xl'>{name} Quiz</h1>
-            {/* <img src={logo} alt="logo" /> */}
-            <p className='text-xl my-10'>Total Questions {total}</p>
+            <div className='flex items-center justify-center mt-10'>
+                <img className='bg-black rounded-full h-24 mr-10' src={logo} alt="logo" />
+                <h1 className='text-6xl'>{name} Quiz</h1>
+            </div>
+            <p className='text-xl mb-10 mt-5'>Total Questions {total}</p>
 
             {
                 questions.map(question => <Quiz key={question.id} quiz={question} id={question.id}></Quiz>)
